4b9d6d92b4fc3f8a0ad6bd3dd5748a7b1b3e8704
swapnasruthi <swapnasruthi2005@gmail.com>
2025-09-06 00:29:11 +0530

    update URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors(
     {
-        origin:"http://localhost:5173",
+        origin:"https://iit-jammu-task.vercel.app",
         methods: ['GET', 'POST','PUT', 'PATCH', 'DELETE'],
         credentials:true
     }
